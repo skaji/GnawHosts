40cfb10d900b1293d8c83bccb82c9ba29ba75844
ks <ks-for-git@live.jp>
2012-08-24 02:12:19 +0900

    remove background.js

---
+++ README.md
@@ -0,0 +1,11 @@
+Gnaw Hosts
+===========
+
+Gnaw Hosts is a Google Chrome App that allows you
+to map some hostnames to IP addresses or others.
+
+You may use this app instead of hosts file.
+In fact you can specify port number of destination hostname or IP address.
+
+### NOTE
+This app cannot map https connection.
--- js/set.js
+++ js/set.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-    var vars = getUrlVars();
-    if (typeof vars['domain'] !== 'undefined') {
-        $('#domain').val(vars['domain']);
-    }
-
     var proxy = localStorage['proxy'];
     $('#proxy').val(proxy);
 
@@ -21,11 +16,24 @@ $(document).ready(function () {
             }
         };
         chrome.proxy.settings.set({
-            value: config, 
+            value: config,
             scope: 'regular'
         }, function() {});
 
-        alert('OK');
+        $('#information')
+        .addClass('alert-success')
+        .text('Success (See details in console)')
+        .show();
+
+        console.log("The specified pac_script is:");
+        console.log(pac);
+        console.log("The currently effective proxy settings are:");
+        chrome.proxy.settings.get(
+            {'incognito': false},
+            function (config) {
+                console.log(config);
+            }
+        );
     });
 });
 
@@ -36,23 +44,30 @@ function hosts2pac(hosts) {
     pac = 'function FindProxyForURL(url, host) {' + "\n";
 
     var lines = hosts.split("\n");
-    lines.forEach(function(line){
+    lines.forEach(function (line) {
         line = line.replace(/#.*/, '');
+        line = line.replace(/\s+$/, '');
 
-        var arr = line.split(/[\s\t]+/);
+        var arr = line.split(/[\s]+/);
         if (arr.length >= 2) {
-            if (arr[1].substr(0, 1) == '.')
-                pac += '    if (dnsDomainIs(host, "' + arr[1] + '"))' + "\n";
-            else
-                pac += '    if (host == "' + arr[1] + '")' + "\n";
+            pac += '    if (';
+            var conditions = [];
+            for (var i = 1; i < arr.length; i += 1) {
+                if (arr[i].substr(0, 1) == '.') {
+                    conditions.push('dnsDomainIs(host, "' + arr[i] + '")');
+                }
+                else {
+                    conditions.push('host == "' + arr[i] + '"');
+                }
+            }
+            pac += conditions.join(' || ');
+            pac += ")\n";
             pac += '        return "PROXY ' + arr[0] + '";' + "\n";
         }
     });
 
     pac += '    return "DIRECT";' + "\n";
     pac += '}';
-
-    console.log(pac);
 
     return pac;
 }
